2968d2a9d6143942e29e724776db3ef481f172a9
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-07 13:59:11 +0200

    Update intro.js

--- data/sombras-del-caribe/cap6/intro.js
+++ data/sombras-del-caribe/cap6/intro.js
@@ -1,5 +1,11 @@
 ChapterLoader.registerContent('intro', `
-*Parpadeáis con pesadez intentado recobrar el sentido, pero un fuerte vértigo os atenaza como si estuvierais a punto de caer por una pendiente. Lentamente os dais cuenta de que yacéis desplomados sobre un suelo cálido e irregular y aunque poco a poco recuperáis la consciencia, el mundo a vuestro alrededor se tambalea y una luz matutina hiere vuestros ojos. Después de un par de intentos infructuosos lográis incorporaros, combatiendo la que seguramente sea la reina de todas las resacas. Los recuerdos regresan con timidez y al mirar en derredor descubrís con estupefacción que el templo ha desaparecido y que, de algún modo, habéis sido abandonados en mitad de la selva.* 
-*Recordáis a vuestros compañeros preguntándoos por su suerte y, no por ello menos importante, sobre qué habrá sido del tesoro. Venciendo vuestro desconcierto, intentáis ubicaros para así salir a la búsqueda de ambos. *
+<div class="reproductor-opcional">
+  <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
+  <audio controls>
+    <source src="audio/cap6.mp3" type="audio/mpeg">
+    Tu navegador no soporta audio HTML5.
+  </audio>
+</div>
+<emParpadeáis con pesadez intentado recobrar el sentido, pero un fuerte vértigo os atenaza como si estuvierais a punto de caer por una pendiente. Lentamente os dais cuenta de que yacéis desplomados sobre un suelo cálido e irregular y aunque poco a poco recuperáis la consciencia, el mundo a vuestro alrededor se tambalea y una luz matutina hiere vuestros ojos. Después de un par de intentos infructuosos lográis incorporaros, combatiendo la que seguramente sea la reina de todas las resacas. Los recuerdos regresan con timidez y al mirar en derredor descubrís con estupefacción que el templo ha desaparecido y que, de algún modo, habéis sido abandonados en mitad de la selva. 
+Recordáis a vuestros compañeros preguntándoos por su suerte y, no por ello menos importante, sobre qué habrá sido del tesoro. Venciendo vuestro desconcierto, intentáis ubicaros para así salir a la búsqueda de ambos.</em>
 `);
-
