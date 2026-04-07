7844c2f7efdb90843151bc7f0529e564818968b6
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-07 14:21:06 +0200

    Update intro.js

--- data/sombras-del-caribe/cap2/intro.js
+++ data/sombras-del-caribe/cap2/intro.js
@@ -2,7 +2,7 @@
 <div class="reproductor-opcional">
 	<p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
 	<audio controls>
-		<source src="audio/intro2.mp3" type="audio/mpeg">
+		<source src="audio/cap2.wav" type="audio/mpeg">
 		Tu navegador no soporta audio HTML5.
 	</audio>
 </div>
