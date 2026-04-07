e37f1c0ff1d96a62c5dc7967dc7d93c575e4ef44
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-07 14:20:43 +0200

    Update intro.js

--- data/sombras-del-caribe/cap3/intro.js
+++ data/sombras-del-caribe/cap3/intro.js
@@ -2,7 +2,7 @@ ChapterLoader.registerContent('intro', `
 <div class="reproductor-opcional">
   <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
   <audio controls>
-    <source src="audio/Intro3.mp3" type="audio/mpeg">
+    <source src="audio/cap3.wav" type="audio/mpeg">
     Tu navegador no soporta audio HTML5.
   </audio>
 </div>
