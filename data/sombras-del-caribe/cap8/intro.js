64d97bdbd512bd3ffd8b54cf79cda862cdbc4d37
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-13 12:15:25 +0200

    Update intro.js

--- data/sombras-del-caribe/cap8/intro.js
+++ data/sombras-del-caribe/cap8/intro.js
@@ -1,4 +1,11 @@
 ChapterLoader.registerContent('intro', `
+<div class="reproductor-opcional">
+  <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
+  <audio controls>
+    <source src="audio/capi8.mp3" type="audio/mpeg">
+    Tu navegador no soporta audio HTML5.
+  </audio>
+</div>
 <p><em>La Cofradía de los Hermanos de la Costa requiere de nuestros servicios para investigar los sucesos acontecidos recientemente.</em></p>
 <p><em>Ya son tres los barcos hundidos desde que abandonamos la isla maldita y los supervivientes hablan de esqueletos que cobran vida y abordan los barcos a medianoche.</em></p>
 <p><em>Navegaremos hasta las diferentes islas donde los barcos han sido atacados misteriosamente. La bruja del consejo, Marie Leaveau, cree que el origen de este mal debe tener alguna relación con el tesoro que encontramos en aquella nefasta isla, y que un gran y oscuro poder está creciendo en el Caribe.</em></p>
