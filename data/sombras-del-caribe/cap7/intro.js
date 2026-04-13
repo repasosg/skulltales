216fe9280bf8854bd529b559ce995cb719e54cca
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-13 12:14:38 +0200

    Update intro.js

--- data/sombras-del-caribe/cap7/intro.js
+++ data/sombras-del-caribe/cap7/intro.js
@@ -1,5 +1,11 @@
 ChapterLoader.registerContent('intro', `
-<p><em>Los piratas no somos conocidos precisamente por nuestro sentido del honor... pero en esta ocasión, cumpliremos el juramento que hicimos a Franky Sands. Según la carta que nos entregó, sus hijos Violette y Luan se encuentran bajo la custodia de su hermano, un Capitán de los Casacas Rojas.<br><br>
-
-Después de permitir descansar a la tripulación durante un par de días y aprovisionarnos para la travesía, nos hacemos a la mar. Me temo que será una difícil empresa...</em></p>
+<div class="reproductor-opcional">
+  <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
+  <audio controls>
+    <source src="audio/capi7.mp3" type="audio/mpeg">
+    Tu navegador no soporta audio HTML5.
+  </audio>
+</div>
+<em> Los piratas no somos conocidos precisamente por nuestro sentido del honor... pero en esta ocasión, cumpliremos el juramento que hicimos a Franky Sands. Según la carta que nos entregó, sus hijos Violette y Luan se encuentran bajo la custodia de su hermano, un Capitán de los Casacas Rojas.<br><br>
+Después de permitir descansar a la tripulación durante un par de días y aprovisionarnos para la travesía, nos hacemos a la mar. Me temo que será una difícil empresa...</em>
 `);
