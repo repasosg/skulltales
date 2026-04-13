ea7879af129c4f7b04c5be759d0b0157fcca99cc
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-13 12:21:32 +0200

    Update intro.js

--- data/sombras-del-caribe/cap5/intro.js
+++ data/sombras-del-caribe/cap5/intro.js
@@ -1,8 +1,4 @@
 ChapterLoader.registerContent('intro', `
-*Paolo Miras nos advirtió del peligro que tenía la isla, pero el mayor tesoro visto por cualquier persona nos aguardaba en ella, y no hay nada más valioso para un pirata que un tesoro. Nos habló de misteriosas voces que se metían en la cabeza y te llevaban a la locura... Pero también mencionó algo sobre un artefacto misterioso que les ayudó a escapar de la isla, la legendaria brújula de Colón.*
-*El tesoro aguarda en el interior del templo oculto dedicado a Yemayá, la diosa del mar, custodiado por el guardián de la cripta. En unas horas, estaremos bañándonos en oro.*
-`);
-ChapterLoader.registerContent('intro', `
 <div class="reproductor-opcional">
   <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
   <audio controls>
