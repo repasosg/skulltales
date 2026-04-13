a2f87ab46ffe9b38fff10ab1f70033c1db1700a2
Isaac Borras <isaacborrasmartinez80@gmail.com>
2026-04-13 12:16:15 +0200

    Update intro.js

--- data/sombras-del-caribe/cap10/intro.js
+++ data/sombras-del-caribe/cap10/intro.js
@@ -1,4 +1,11 @@
 ChapterLoader.registerContent('intro', `
+<div class="reproductor-opcional">
+  <p class="audio-titulo">🎧 <strong>Audio Narrativo:</strong></p>
+  <audio controls>
+    <source src="audio/capi10.mp3" type="audio/mpeg">
+    Tu navegador no soporta audio HTML5.
+  </audio>
+</div>
 <p><em>"Cuando la última luz del día sea devorada por la oscuridad, Oggun renacerá de su espíritu con todo su poder..." Fueron las últimas palabras de Marie Laveau antes de embarcarnos a la que podría ser nuestra última aventura. Había llegado la hora de la verdad, de dejar a un lado nuestros intereses particulares y defender la libertad por la que siempre hemos luchado. Ni hombres de mar, ni soldados sin patria, ni ladrones de esperanzas, ni caballeros sin honor... solo piratas contra una nueva amenaza para el mundo. Mientras avistamos la costa, se entona la última canción pirata...</em></p>
 <p><em>El espíritu de Oggun ha regresado a la isla de las Almas Olvidadas y dispone de una horda de seres sobrenaturales. Debemos encontrar el Altar de la Luz y los cuatro elementos para atrapar su forma espiritual en un cuerpo físico, usando la pistola de Davy Jones para derrotar al vengativo Oggun para siempre.</em></p>
 `);
